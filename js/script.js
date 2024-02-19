1275dc026267e7f5bea7b38dcbcdd545a6928667
hrodriguez007 <heidyrodriguez567@gmail.com>
2024-02-19 14:18:24 -0600

    Upload files to github

---
+++ js/script.js
@@ -0,0 +1,155 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+// })
+
+//pick out all divs inside the div with class grid
+const squares = document.querySelectorAll('.grid div');
+const score = document.querySelector('#score');
+const displayCurrentPlayer = document.querySelector('#current-player');
+//let js know who current player is
+let currentPlayer = 1;
+
+const winningArrays = [
+    [0, 1, 2, 3],
+    [41, 40, 39, 38],
+    [7, 8, 9, 10],
+    [34, 33, 32, 31],
+    [14, 15, 16, 17],
+    [27, 26, 25, 24],
+    [21, 22, 23, 24],
+    [20, 19, 18, 17],
+    [28, 29, 30, 31],
+    [13, 12, 11, 10],
+    [35, 36, 37, 38],
+    [6, 5, 4, 3],
+    [0, 7, 14, 21],
+    [41, 34, 27, 20],
+    [1, 8, 15, 22],
+    [40, 33, 26, 19],
+    [2, 9, 16, 23],
+    [39, 32, 25, 18],
+    [3, 10, 17, 24],
+    [38, 31, 24, 17],
+    [4, 11, 18, 25],
+    [37, 30, 23, 16],
+    [5, 12, 19, 26],
+    [36, 29, 22, 15],
+    [6, 13, 20, 27],
+    [35, 28, 21, 14],
+    [0, 8, 16, 24],
+    [41, 33, 25, 17],
+    [7, 15, 23, 31],
+    [34, 26, 18, 10],
+    [14, 22, 30, 38],
+    [27, 19, 11, 3],
+    [35, 29, 23, 17],
+    [6, 12, 18, 24],
+    [28, 22, 16, 10],
+    [13, 19, 25, 31],
+    [21, 15, 9, 3],
+    [20, 26, 32, 38],
+    [36, 30, 24, 18],
+    [5, 11, 17, 23],
+    [37, 31, 25, 19],
+    [4, 10, 16, 22],
+    [2, 10, 18, 26],
+    [39, 31, 23, 15],
+    [1, 9, 17, 25],
+    [40, 32, 24, 16],
+    [9, 17, 25, 33],
+    [8, 16, 24, 32],
+    [11, 17, 23, 29],
+    [12, 18, 24, 30],
+    [1, 2, 3, 4],
+    [5, 4, 3, 2],
+    [8, 9, 10, 11],
+    [12, 11, 10, 9],
+    [15, 16, 17, 18],
+    [19, 18, 17, 16],
+    [22, 23, 24, 25],
+    [26, 25, 24, 23],
+    [29, 30, 31, 32],
+    [33, 32, 31, 30],
+    [36, 37, 38, 39],
+    [40, 39, 38, 37],
+    [7, 14, 21, 28],
+    [8, 15, 22, 29],
+    [9, 16, 23, 30],
+    [10, 17, 24, 31],
+    [11, 18, 25, 32],
+    [12, 19, 26, 33],
+    [13, 20, 27, 34],
+]
+
+//fucntion to check the score
+function checkBoard() {
+    for (let i = 0; i < winningArrays.length; i++) {
+        //for each time loop, save square1, square2, square3, etc
+        //into squares array and pass thru to winningArrays to get to first combination [0,1,2,3]
+        //return to index 0
+        const square1 = squares[winningArrays[i][0]];
+        //returning back index 1
+        const square2 = squares[winningArrays[i][1]];
+        //returning back index 2
+        const square3 = squares[winningArrays[i][2]];
+        //returning back index 4
+        const square4 = squares[winningArrays[i][3]];
+
+        //check that squares for the class of player-1
+    //if the square1, square2, sqaure3, square4 contains the player-1
+        if (
+            square1.classList.contains('player-1') &&
+            square2.classList.contains('player-1') &&
+            square3.classList.contains('player-1') &&
+            square4.classList.contains('player-1')
+        ) {
+            //then need to get the score
+            score.innerHTML = "Player One Wins!"
+        }
+        //check that squares for the class of player-1
+        //if the square1, square2, sqaure3, square4 contains the player-1
+        if (
+            square1.classList.contains('player-2') &&
+            square2.classList.contains('player-2') &&
+            square3.classList.contains('player-2') &&
+            square4.classList.contains('player-2')
+        ) {
+            //then need to get the score
+            score.innerHTML = "Player Two Wins!"
+        }
+    }
+    
+}
+
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+        //everytime a click happens this alert shows
+        // alert('You have clicked square ' + i)
+        //square below filled, then go on top of it
+        
+        //if we go into our squares array, plus 7 to
+        //directly check the square below the current one
+        //if that square contains 'taken' we can go
+        //if true and square is taken, if the current player is 
+        //player-1 then to that square add 'taken' and 'player-one'
+        //then change to player-2
+        //display the player change at end
+        
+        if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')){//add players only if space not currently taken
+            if (currentPlayer == 1) {
+                squares[i].classList.add('taken');
+                squares[i].classList.add('player-1');
+                currentPlayer = 2;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+                //logic of player-2 is same as above player-2 and currentPlayer = 1 instead
+            } else if (currentPlayer == 2) {
+                squares[i].classList.add('taken');
+                squares[i].classList.add('player-2');
+                currentPlayer = 1;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+            }
+         } else alert('Not Allowed. Try Again.');
+         checkBoard();
+    }
+}
